8e8704284fc4707070495e4021d7110dbbbf3adb
AlimuhammadF <Alimuhammadfp5@gmail.com>
2022-08-02 15:08:59 -0700

    auth screen

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
-import '../styles/globals.css'
+import Layout from "../components/Layout";
+import "../styles/globals.css";
+import "../styles/nprogress.css";
+import { SessionProvider } from "next-auth/react";
+import Router from "next/router";
+import nProgress from "nprogress";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+// nprogress bar
+Router.events.on("routeChangeStart", nProgress.start);
+Router.events.on("routeChangeComplete", nProgress.done);
+Router.events.on("routeChangeError", nProgress.done);
+
+export default function App({
+	Component,
+	pageProps: { session, ...pageProps },
+}) {
+	return (
+		<SessionProvider session={session}>
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
+		</SessionProvider>
+	);
 }
-
-export default MyApp
